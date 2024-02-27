8502ff7c30c303fde5cff27d7cf522316e7482c5
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-02-27 18:05:57 +0100

    refactor googleMaps loading

--- src/Components/GoogleMaps/GoogleMaps.jsx
+++ src/Components/GoogleMaps/GoogleMaps.jsx
@@ -3,12 +3,13 @@ import "./GoogleMaps.css";
 
 const GoogleMaps = () => {
   const [isMapLoaded, setIsMapLoaded] = useState(false);
+  const [link, setLink] = useState("");
 
   useEffect(() => {
+    setIsMapLoaded(false);
     handleMapLoad();
   }, []);
 
-  const [link, setLink] = useState("");
   function handleMapLoad() {
     setLink(
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1488.2855564202175!2d20.384698427983984!3d45.338159537911395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475add5431a87b1b%3A0x96e59aa3279277f3!2sadexto%20doo!5e1!3m2!1shu!2shu!4v1702730565578!5m2!1shu!2shu"
