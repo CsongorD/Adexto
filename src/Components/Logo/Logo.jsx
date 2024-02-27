703c59b49962edb1ae3a35b3651c39ad3644b08e
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-02-27 18:15:43 +0100

    change imageComponent to img in logo component

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,20 +1,13 @@
-// import { NavLink } from "react-router-dom";
 import "boxicons";
 import "./Logo.css";
 import logo from "../../assets/db/logo_db.json";
-import ImageComponent from "../ImageComponent/ImageComponent";
 import { NavLink } from "react-router-dom";
 
 const Logo = () => {
   return (
     <NavLink className={"logo"} to={"/"}>
       <div className="logo-image">
-        <ImageComponent
-          src={logo[0].path}
-          alt="adexto-logo"
-          loading={"lazy"}
-          small={logo[0].small}
-        />
+        <img src={logo[0].path} alt="adexto-logo" />
       </div>
       <h2>ADEXTO</h2>
     </NavLink>
