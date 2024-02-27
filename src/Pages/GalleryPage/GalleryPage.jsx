fb60bbdf2bcc079ec5915d61a8c529de473c2daa
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-02-27 18:04:04 +0100

    remove queryClient

--- src/Pages/GalleryPage/GalleryPage.jsx
+++ src/Pages/GalleryPage/GalleryPage.jsx
@@ -1,17 +1,13 @@
 import "./GalleryPage.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Header from "../../Components/Header/Header";
 import Gallery from "../../Components/Gallery/Gallery";
 import Footer from "../../Components/Footer/Footer";
 
 const GalleryPage = () => {
-  const queryClient = new QueryClient();
   return (
     <div className="gallery-page">
       <Header />
-      <QueryClientProvider client={queryClient}>
-        <Gallery />
-      </QueryClientProvider>
+      <Gallery />
       <Footer />
     </div>
   );
