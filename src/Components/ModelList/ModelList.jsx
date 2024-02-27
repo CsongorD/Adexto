d5a7255f0eb956c377054f0d340680d5a044690e
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-02-27 18:01:51 +0100

    fix model slider logic

--- src/Components/ModelList/ModelList.jsx
+++ src/Components/ModelList/ModelList.jsx
@@ -3,59 +3,65 @@ import React, { useEffect, useState } from "react";
 import "./ModelList.css";
 import ImageComponent from "../ImageComponent/ImageComponent";
 
-const ModelList = ({ models, paginate, currentPage, loading }) => {
+const ModelList = ({ model, paginate, currentPage }) => {
   const [imageIndex, setImageIndex] = useState(0);
-  const [direction, setDirection] = useState(false);
-
-  let imgInd = direction ? 0 : models[0].images.length - 1;
-
+  const [mod, setMod] = useState(null);
+  const [isPrev, setIsPrev] = useState(false);
   useEffect(() => {
-    setImageIndex(imgInd);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPage]);
+    setMod(model);
 
-  let model = models[0];
+    if (isPrev) {
+      setImageIndex(model.images.length - 1);
+    } else {
+      setImageIndex(0);
+    }
+  }, [currentPage, model, isPrev]);
 
-  let modelNumber = model.model;
-  let modelImages = model.images;
+  if (!mod || !mod.images || !mod.model) {
+    return <div>Model data is missing!</div>;
+  }
+
+  if (mod.images.length === 0) {
+    return <div>No images found for this model!</div>;
+  }
+
+  let modelNumber = mod?.model;
+  let modelImages = mod?.images;
+  let currentImage = modelImages[imageIndex];
+  let currentImagePath = currentImage.path;
+  let currentImageSmall = currentImage.small;
 
   function showPrevImage() {
     if (imageIndex === 0) {
-      setDirection(false);
       paginate(currentPage - 1);
+      setIsPrev(true);
     } else {
-      setImageIndex(imageIndex - 1);
+      setImageIndex((prevIndex) => prevIndex - 1);
     }
   }
-
   function showNextImage() {
     if (imageIndex === modelImages.length - 1) {
-      setDirection(true);
       paginate(currentPage + 1);
+      setIsPrev(false);
     } else {
-      setImageIndex(imageIndex + 1);
+      setImageIndex((prevIndex) => prevIndex + 1);
     }
   }
-  const currentImage = modelImages[imageIndex];
-
   return (
     <div className="models-container">
       <h1 className="title">Model {modelNumber}</h1>
-
       <div className="image-slider">
         <div className="image-slider-btn prev" onClick={() => showPrevImage()}>
           <box-icon id="arrow-btn" name="chevron-left" type="solid"></box-icon>
         </div>
-
         <div className="image-slider-img">
           <ImageComponent
-            src={currentImage.path}
-            small={currentImage.small}
-            alt={"img-" + { modelNumber }}
-            loading="lazy"
+            src={currentImagePath}
+            small={currentImageSmall}
+            alt={"img-" + modelNumber}
+            loading={"lazy"}
           />
         </div>
-
         <div className="image-slider-btn next" onClick={() => showNextImage()}>
           <box-icon id="arrow-btn" type="solid" name="chevron-right"></box-icon>
         </div>
