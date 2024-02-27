9af3b47e89459e96aa470bebaec83a3ad9a0455e
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-02-27 17:58:44 +0100

    remove unused code

--- src/Components/ColumnTable/ColumnTable.jsx
+++ src/Components/ColumnTable/ColumnTable.jsx
@@ -3,8 +3,7 @@ import "./ColumnTable.css";
 import { forwardRef } from "react";
 
 const columns = require("../../assets/db/column_db.json");
-// eslint-disable-next-line no-empty-pattern
-const ColumnTable = ({}, ref) => {
+const ColumnTable = forwardRef((_props, ref) => {
   return (
     <div className="column-table" ref={ref}>
       <h1 className="title">STUBOVI</h1>
@@ -40,37 +39,8 @@ const ColumnTable = ({}, ref) => {
           );
         })}
       </div>
-      {/* <div className="column-list">
-        <h3>Stub na 1.5m</h3>
-        <h4>Ukupna duzina 2.1m</h4>
-        <h2>12€</h2>
-        <br />
-        <h3>Stub na 2.0m</h3>
-        <h4>Ukupna duzina 2.8m</h4>
-        <h2>14€</h2>
-        <br />
-        <h3>Stub na 2.5m</h3>
-        <h4>Ukupna duzina 3.3m</h4>
-        <h2>28€</h2>
-        <br />
-        <br />
-        <h3>Stub zaversni 1.5m</h3>
-        <h2>14€</h2>
-        <br />
-        <h3>Stub zaversni 2.0m</h3>
-        <h2>16€</h2>
-        <br />
-        <br />
-
-        <h4>Stub ukrasni sa kapom na 1.5m visine</h4>
-        <h2>18€</h2>
-        <br />
-
-        <h4>Stub ukrasni sa kapom na 2.0m visine</h4>
-        <h2>23€</h2>
-      </div> */}
     </div>
   );
-};
+});
 
-export default forwardRef(ColumnTable);
+export default ColumnTable;
