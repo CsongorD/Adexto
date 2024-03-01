600b213472a610569abb10cf285784d995e27ce5
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-03-01 13:22:27 +0100

    change loading component to spinner

--- src/Components/FenceModel/FenceModel.jsx
+++ src/Components/FenceModel/FenceModel.jsx
@@ -1,12 +1,13 @@
+import Spinner from "../Spinner/Spinner";
 import "./FenceModel.css";
-import React, { lazy, Suspense } from "react";
+import { lazy, Suspense } from "react";
 
 const Spline = lazy(() => import("@splinetool/react-spline"));
 
 const FenceModel = () => {
   return (
     <div className="model">
-      <Suspense fallback={<div>Loading</div>}>
+      <Suspense fallback={<Spinner />}>
         <Spline scene="https://prod.spline.design/dCHi5qcJbR3olvzV/scene.splinecode" />
       </Suspense>
     </div>
--- src/Components/GoogleMaps/GoogleMaps.jsx
+++ src/Components/GoogleMaps/GoogleMaps.jsx
@@ -1,40 +1,40 @@
-import { useEffect, useState } from "react";
 import "./GoogleMaps.css";
+import { useEffect, useState } from "react";
+import Spinner from "../Spinner/Spinner";
 
 const GoogleMaps = () => {
-  const [isMapLoaded, setIsMapLoaded] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [link, setLink] = useState("");
 
   useEffect(() => {
-    setIsMapLoaded(false);
-    handleMapLoad();
+    fetchMap().then((res) => {
+      setLink(res);
+      setLoading(false);
+    });
   }, []);
 
-  function handleMapLoad() {
-    setLink(
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1488.2855564202175!2d20.384698427983984!3d45.338159537911395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475add5431a87b1b%3A0x96e59aa3279277f3!2sadexto%20doo!5e1!3m2!1shu!2shu!4v1702730565578!5m2!1shu!2shu"
-    );
-    setIsMapLoaded(true);
+  async function fetchMap() {
+    let url =
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1488.2855564202175!2d20.384698427983984!3d45.338159537911395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475add5431a87b1b%3A0x96e59aa3279277f3!2sadexto%20doo!5e1!3m2!1shu!2shu!4v1702730565578!5m2!1shu!2shu";
+
+    return url;
   }
 
   return (
-    <>
-      {isMapLoaded ? (
-        <div className="map-container">
-          <iframe
-            className="maps"
-            title="Google Maps"
-            src={link}
-            allowFullScreen={false}
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            onLoad={handleMapLoad}
-          ></iframe>
-        </div>
+    <div className="map-container">
+      {loading ? (
+        <Spinner />
       ) : (
-        <div>Loading...</div>
+        <iframe
+          className="maps"
+          title="Google Maps"
+          src={link}
+          allowFullScreen={false}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       )}
-    </>
+    </div>
   );
 };
 
