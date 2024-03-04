1bc92506196715ef97795ae3a3d07eba05a1bd8b
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-03-04 19:44:53 +0100

    modify import paths

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
 import RootLayout from "./layouts/RootLayout/RootLayout";
+import Loading from "./components/Loading/Loading";
 
-import NotFoundPage from "./pages/NotFoundPage";
-import HomePage from "./pages/HomePage";
-import ModelsPage from "./pages/ModelsPage";
-import TypesPage from "./pages/TypesPage";
-import ContactPage from "./pages/ContactPage";
-import GalleryPage from "./pages/GalleryPage";
+import HomePage from "./pages/HomePage/HomePage";
+const ModelsPage = lazy(() => import("./pages/ModelsPage/ModelsPage"));
+const TypesPage = lazy(() => import("./pages/TypesPage/TypesPage"));
+const ContactPage = lazy(() => import("./pages/ContactPage/ContactPage"));
+const GalleryPage = lazy(() => import("./pages/GalleryPage/GalleryPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
 const router = createBrowserRouter([
   {
@@ -15,11 +17,46 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { path: "/", element: <HomePage /> },
-      { path: "/betonske-ograde", element: <ModelsPage /> },
-      { path: "/cenovnik", element: <TypesPage /> },
-      { path: "/galerija", element: <GalleryPage /> },
-      { path: "/kontakt", element: <ContactPage /> },
-      { path: "*", element: <NotFoundPage /> },
+      {
+        path: "/betonske-ograde",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <ModelsPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/cenovnik",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <TypesPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/galerija",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <GalleryPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/kontakt",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <ContactPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "*",
+        element: (
+          <Suspense fallback={<Loading />}>
+            <NotFoundPage />
+          </Suspense>
+        ),
+      },
     ],
   },
 ]);
