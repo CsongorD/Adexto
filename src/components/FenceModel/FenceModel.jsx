7a333b4040237faa8c69af1ac62de01b422b6420
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-03-06 09:07:35 +0100

    change logic of fencemodel loading

--- src/components/FenceModel/FenceModel.jsx
+++ src/components/FenceModel/FenceModel.jsx
@@ -1,15 +1,27 @@
-import Spinner from "../Spinner/Spinner";
+import { useEffect, useState } from "react";
 import "./FenceModel.css";
-import { lazy, Suspense } from "react";
 
-const Spline = lazy(() => import("@splinetool/react-spline"));
+import Spline from "@splinetool/react-spline";
+import Spinner from "../../components/Spinner/Spinner";
 
 const FenceModel = () => {
+  const [model, setModel] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    handleLoad();
+  }, []);
+  function handleLoad() {
+    setModel("https://prod.spline.design/dCHi5qcJbR3olvzV/scene.splinecode");
+    setLoading(false);
+  }
   return (
     <div className="model">
-      <Suspense fallback={<Spinner />}>
-        <Spline scene="https://prod.spline.design/dCHi5qcJbR3olvzV/scene.splinecode" />
-      </Suspense>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Spline scene={model} onLoad={() => handleLoad()} />
+      )}
     </div>
   );
 };
