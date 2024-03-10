227db42107064127e9dad58ec5854388af9b6c32
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-03-10 21:04:11 +0100

    create column component

---
+++ src/components/Column/Column.jsx
@@ -0,0 +1,27 @@
+import "./Column.css";
+import Image from "../Image/Image";
+
+const Column = ({ column }) => {
+  const { name, path, small, options } = column;
+  return (
+    <div className="column-container">
+      <h2 className="column-title">{name}</h2>
+      <div className="column-image">
+        <Image src={path} alt={name} small={small} loading="lazy" />
+      </div>
+      <div className="column-lengths">
+        <h3>DUŽINE:</h3>
+        {options.map((option, index) => (
+          <div key={index} className="column-length">
+            <h3>
+              {`${option.length.toFixed(1)}m (ukupna dužina: ${
+                option.full_length
+              }m) => ${option.price}€`}
+            </h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Column;
