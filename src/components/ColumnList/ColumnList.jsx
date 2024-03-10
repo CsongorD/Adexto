8a6ea21e71880de24fe0b96fa00ecd590c3cffa8
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-03-10 21:05:08 +0100

    create column list component

---
+++ src/components/ColumnList/ColumnList.jsx
@@ -0,0 +1,24 @@
+import "./ColumnList.css";
+import useImages from "../../hooks/useImages";
+import Column from "../Column/Column";
+import ImageLoading from "../ImageLoading/ImageLoading";
+import ErrorComponent from "../ErrorComponent/ErrorComponent";
+
+const ColumnList = () => {
+  const [columns, error] = useImages("column");
+
+  if (error) {
+    return <ErrorComponent error={error.message} />;
+  }
+
+  return !columns ? (
+    <ImageLoading />
+  ) : (
+    <div className="column-list">
+      {columns?.map((column, index) => (
+        <Column column={column} key={index} />
+      ))}
+    </div>
+  );
+};
+export default ColumnList;
