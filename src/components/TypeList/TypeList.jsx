1a3a4a3adce484ee0847882059c434e929633106
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-03-10 21:07:51 +0100

    create typelist component

---
+++ src/components/TypeList/TypeList.jsx
@@ -0,0 +1,26 @@
+import "./TypeList.css";
+import Type from "../Type/Type";
+import useImages from "../../hooks/useImages";
+import ErrorComponent from "../ErrorComponent/ErrorComponent";
+import ImageLoading from "../ImageLoading/ImageLoading";
+
+const TypeList = () => {
+  const [types, error] = useImages("type");
+
+  if (error) {
+    return <ErrorComponent error={error.message} />;
+  }
+
+  if (!types) {
+    return <ImageLoading />;
+  }
+
+  return (
+    <div className="type-list">
+      {types?.map((type, index) => (
+        <Type type={type} key={index} />
+      ))}
+    </div>
+  );
+};
+export default TypeList;
