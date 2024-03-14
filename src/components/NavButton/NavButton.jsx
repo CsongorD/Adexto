b11ce6dc61c51eafbeb30a259b94d10ba2651625
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-03-14 11:09:27 +0100

    create navbutton

---
+++ src/components/NavButton/NavButton.jsx
@@ -0,0 +1,17 @@
+import "./NavButton.css";
+import { NavLink } from "react-router-dom";
+
+const NavButton = ({ to, text, onClick, className }) => {
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+    onClick();
+  };
+
+  return (
+    <NavLink className={className} to={to} onClick={handleClick}>
+      {text}
+    </NavLink>
+  );
+};
+
+export default NavButton;
