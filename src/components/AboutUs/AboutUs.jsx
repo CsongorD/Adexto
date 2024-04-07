f505c16d00fc3935d0fae8b82cea41fab7784523
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-04-07 20:24:25 +0200

    add role to about us titles

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,8 +1,8 @@
-import "./AboutUs.css";
 import useImages from "../../hooks/useImages";
-import MedalList from "../MedalList/MedalList";
-import ImageLoading from "../ImageLoading/ImageLoading";
 import ErrorComponent from "../ErrorComponent/ErrorComponent";
+import ImageLoading from "../ImageLoading/ImageLoading";
+import MedalList from "../MedalList/MedalList";
+import "./AboutUs.css";
 
 const AboutUs = () => {
   const [medals, error] = useImages("medal");
@@ -22,7 +22,7 @@ const AboutUs = () => {
     <div className="about-us">
       {renderMedalList(medals, 0, 3)}
       <div className="about-us-container">
-        <h1>INFORMACIJE O NAMA</h1>
+        <h1 role="presentation">INFORMACIJE O NAMA</h1>
         <p>
           Preduzeće Adexto d.o.o osnovano je 2007 godine u Zrenjaninu. Naša
           firma je prva na teritoriji balkana počela sa izradom betonskih
@@ -30,12 +30,12 @@ const AboutUs = () => {
           izdržljivosti i trajnosti, a i jedini smo dobitnici sertifikata "FIRMA
           OD POVERENJA".
         </p>
-        <h2>Naš poslovni koncept</h2>
+        <h2 role="presentation">Naš poslovni koncept</h2>
         <p>
           Adexto će na jednostavan način ponuditi prodaju i postavljanje
           betonskih ograda.
         </p>
-        <h2>Naša vizija</h2>
+        <h2 role="presentation">Naša vizija</h2>
         <p>
           Želja nam je da vam približimo novine u betonskoj proizvodnji kroz
           asortiman naših proizvoda, i trudimo se da budemo uvek još bolji.
