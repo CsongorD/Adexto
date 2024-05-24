037f4c0e8f07628b0f98c81eda908e6652b22790
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2024-05-24 08:58:39 +0200

    change google maps lang

--- src/components/GoogleMaps/GoogleMaps.jsx
+++ src/components/GoogleMaps/GoogleMaps.jsx
@@ -13,7 +13,7 @@ const GoogleMaps = () => {
   const fetchMapLink = async () => {
     try {
       const res =
-        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1488.2855564202175!2d20.384698427983984!3d45.338159537911395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475add5431a87b1b%3A0x96e59aa3279277f3!2sadexto%20doo!5e1!3m2!1shu!2shu!4v1702730565578!5m2!1shu!2shu";
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2537.9577581709364!2d20.383021975752488!3d45.33801934184959!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475add5431a87b1b%3A0x96e59aa3279277f3!2sadexto%20doo!5e1!3m2!1shu!2srs!4v1716533845705!5m2!1shu!2srs";
       setMapLink(res);
       setIsLoading(false);
     } catch (error) {
