f744b37b34f38361abffbe3850bb533f389f5427
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-03-14 20:27:34 +0100

    migrate column component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,18 +1,18 @@
-import "./Column.css";
+import styles from "./Column.module.css";
 import Image from "../Image/Image";
 
 const Column = ({ column }) => {
   const { name, path, small, options } = column;
   return (
-    <div className="column-container">
-      <h2 className="column-title">{name}</h2>
-      <div className="column-image">
+    <div className={styles["column-container"]}>
+      <h2 className={styles["column-title"]}>{name}</h2>
+      <div className={styles["column-image"]}>
         <Image src={path} alt={name} small={small} loading="lazy" />
       </div>
-      <div className="column-lengths">
+      <div className={styles["column-lengths"]}>
         <h3>DUŽINE:</h3>
         {options.map((option, index) => (
-          <div key={index} className="column-length">
+          <div key={index} className={styles["column-length"]}>
             <p>
               {`${option.length.toFixed(1)}m (ukupna dužina: ${
                 option.full_length
