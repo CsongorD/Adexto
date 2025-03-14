6f915831ab9da9ed220994b427fd92aa08f71061
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-03-14 20:35:22 +0100

    migrate logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,38 @@
+"use client";
+import useImages from "../../hooks/useImages";
+import Image from "../Image/Image";
+import ImageLoading from "../ImageLoading/ImageLoading";
+import NavButton from "../NavButton/NavButton";
+import styles from "./Logo.module.css";
+
+const Logo = () => {
+  const [logo, error] = useImages("logo");
+
+  if (error) throw error;
+  if (!logo) return <ImageLoading />;
+
+  return (
+    <div className={styles["logo-container"]}>
+      <NavButton
+        className={styles["logo"]}
+        to="/"
+        text={
+          <>
+            <div className={styles["logo-image"]}>
+              <Image
+                src={logo[0]?.path}
+                small={logo[0]?.small}
+                alt="adexto-logo"
+                priority={true}
+                width={100}
+                height={100}
+              />
+            </div>
+            <h2>ADEXTO</h2>
+          </>
+        }
+      />
+    </div>
+  );
+};
+export default Logo;
