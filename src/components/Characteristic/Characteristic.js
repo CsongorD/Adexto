f2729b87f8d1e919a0f9741607d37931fc43d09e
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-03-14 20:26:56 +0100

    migrate characteristic component

--- src/components/Characteristic/Characteristic.js
+++ src/components/Characteristic/Characteristic.js
@@ -1,11 +1,11 @@
-import "./Characteristic.css";
 import Image from "../Image/Image";
+import styles from "./Characteristic.module.css";
 
 const Characteristic = ({ characteristic }) => {
   const { path, text, small } = characteristic;
   return (
-    <div className="characteristic-container">
-      <div className="characteristic-image">
+    <div className={styles["characteristic-container"]}>
+      <div className={styles["characteristic-image"]}>
         <Image src={path} alt={text + "-logo"} small={small} loading={"lazy"} />
       </div>
       <p>{text}</p>
