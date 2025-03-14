037d7279ac2fc6b24d696e1a065cb5f5dfba605e
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-03-14 20:39:45 +0100

    migrate typelist component

---
+++ src/components/TypeList/TypeList.js
@@ -0,0 +1,21 @@
+import useImages from "../../hooks/useImages";
+import ImageLoading from "../ImageLoading/ImageLoading";
+import Type from "../Type/Type";
+import styles from "./TypeList.module.css";
+
+const TypeList = () => {
+  const [types, error] = useImages("type");
+
+  if (error) throw error;
+  if (!types) return <ImageLoading />;
+
+  return (
+    <div className={styles["type-list"]}>
+      {types.map((type, index) => (
+        <Type key={type.id || index} type={type} index={index} />
+      ))}
+    </div>
+  );
+};
+
+export default TypeList;
