8f730c0c641a924f66606877f94f70023c5d7555
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-03-14 20:28:04 +0100

    migrate columntable component

--- src/components/ColumnTable/ColumnTable.js
+++ src/components/ColumnTable/ColumnTable.js
@@ -1,11 +1,11 @@
-import "./ColumnTable.css";
 import { forwardRef } from "react";
 import ColumnList from "../ColumnList/ColumnList";
+import styles from "./ColumnTable.module.css";
 
 const ColumnTable = forwardRef((_props, ref) => {
   return (
-    <div className="column-table" ref={ref}>
-      <h1 className="title">STUBOVI</h1>
+    <div className={styles["column-table"]} ref={ref}>
+      <h1 className={styles.title}>STUBOVI</h1>
       <ColumnList />
     </div>
   );
