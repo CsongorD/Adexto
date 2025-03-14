742e7577beeab0921a910a035489a6542010bcf5
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-03-14 20:34:18 +0100

    migrate imageslider component

---
+++ src/components/ImageSlider/ImageSlider.js
@@ -0,0 +1,41 @@
+import NextIcon from "../Icons/NextIcon";
+import PrevIcon from "../Icons/PrevIcon";
+import Image from "../Image/Image";
+import styles from "./ImageSlider.module.css";
+
+const ImageSlider = ({
+  currentImage,
+  modelNumber,
+  handleLoad,
+  showPrevImage,
+  showNextImage,
+}) => {
+  const { path, small } = currentImage;
+
+  return (
+    <div className={styles["image-slider"]}>
+      <div
+        className={styles["image-slider-button"] + " " + styles["prev"]}
+        onClick={showPrevImage}
+      >
+        <PrevIcon className={styles["arrow-icon"]} />
+      </div>
+      <div className={styles["image-slider-image"]}>
+        <Image
+          src={path}
+          small={small}
+          alt={"img-" + modelNumber}
+          loading="eager"
+          onLoad={handleLoad}
+        />
+      </div>
+      <div
+        className={styles["image-slider-button"] + " " + styles["next"]}
+        onClick={showNextImage}
+      >
+        <NextIcon className={styles["arrow-icon"]} />
+      </div>
+    </div>
+  );
+};
+export default ImageSlider;
