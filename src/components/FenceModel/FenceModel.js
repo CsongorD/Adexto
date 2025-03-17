a79568ceeb683e336e1ed3a78dadb7044d91e04e
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-03-17 07:42:32 +0100

    change fencemodel from spline object to video

--- src/components/FenceModel/FenceModel.js
+++ src/components/FenceModel/FenceModel.js
@@ -1,29 +1,11 @@
-"use client";
-
-import { Suspense, lazy, useEffect, useState } from "react";
-import ImageLoading from "../../components/ImageLoading/ImageLoading";
 import styles from "./FenceModel.module.css";
 
-const Spline = lazy(() => import("@splinetool/react-spline"));
-
 const FenceModel = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [modelUrl, setModelUrl] = useState(null);
-
-  useEffect(() => {
-    setModelUrl("https://prod.spline.design/dCHi5qcJbR3olvzV/scene.splinecode");
-    setIsLoading(false);
-  }, []);
-
   return (
     <div className={styles["fence-model-container"]}>
-      {isLoading ? (
-        <ImageLoading />
-      ) : (
-        <Suspense fallback={<ImageLoading />}>
-          <Spline scene={modelUrl} />
-        </Suspense>
-      )}
+      <video autoPlay>
+        <source src="/adexto-3-d-model.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 };
