626afd5061da953ef39be9c2f3153390c5f1305a
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-04-01 21:22:04 +0200

    update galerija page metadata

--- src/app/galerija/page.js
+++ src/app/galerija/page.js
@@ -1,12 +1,15 @@
 import Gallery from "../../components/Gallery/Gallery";
 
 export const metadata = {
-  title: "Galerija",
-  description: "Pogledajte slike naših betonskih ograda.",
-  keywords: "betonske ograde, galerija, slike, Zrenjanin, Adexto",
+  title: "Galerija - Betonske Ograde Zrenjanin",
+  description:
+    "Pogledajte galeriju slika naših betonskih ograda u Zrenjaninu. Inspirišite se dizajnom i kvalitetom naših proizvoda.",
+  keywords:
+    "betonske ograde galerija, slike betonskih ograda, galerija Zrenjanin, betonske ograde Adexto, dizajn ograda",
   openGraph: {
-    title: "Galerija - Adexto",
-    description: "Pogledajte slike naših betonskih ograda.",
+    title: "Galerija - Betonske Ograde Zrenjanin | Adexto",
+    description:
+      "Pogledajte galeriju slika naših betonskih ograda u Zrenjaninu. Inspirišite se dizajnom i kvalitetom naših proizvoda.",
     url: "https://adexto.com/galerija",
     siteName: "Adexto",
     type: "website",
