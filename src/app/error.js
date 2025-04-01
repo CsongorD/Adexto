4393d2c792db313862b572dcc6a91026f3ed1c36
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-04-01 21:20:36 +0200

    modify error component

--- src/app/error.js
+++ src/app/error.js
@@ -1,11 +1,13 @@
 "use client"; // Ensures it's a client component
-
+import styles from "../styles/error.module.css"; // Adjust the path as necessary
 export default function GlobalError({ error, reset }) {
   return (
-    <div style={{ padding: "20px", textAlign: "center", color: "red" }}>
-      <h2>Greška: Nešto je pošlo po zlu!</h2>
-      <p>{error?.message || "Pokušajte ponovo kasnije."}</p>
-      <button onClick={() => reset()}>Pokušajte ponovo</button>
+    <div className={styles["error-container"]}>
+      <div className={styles["error-message"]}>
+        <h2>Error: Something went wrong!</h2>
+        <p>{error?.message || "Pokušajte ponovo kasnije."}</p>
+        <button onClick={() => reset()}>Try again</button>
+      </div>
     </div>
   );
 }
