eff3f568ba725383e3414f13df72ad08ee5c9eaf
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-04-01 21:25:18 +0200

    add muted attribute to video tag

--- src/components/FenceModel/FenceModel.js
+++ src/components/FenceModel/FenceModel.js
@@ -3,7 +3,7 @@ import styles from "./FenceModel.module.css";
 const FenceModel = () => {
   return (
     <div className={styles["fence-model-container"]}>
-      <video autoPlay>
+      <video autoPlay muted>
         <source src="/adexto-3-d-model.mp4" type="video/mp4" />
       </video>
     </div>
