6199c0507e92adce61d2b8c6729ae4f8fc1fc354
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-04-01 21:22:16 +0200

    update kontakt page metadata

--- src/app/kontakt/page.js
+++ src/app/kontakt/page.js
@@ -3,12 +3,15 @@ import GoogleMaps from "../../components/GoogleMaps/GoogleMaps";
 import styles from "../../styles/contact.module.css";
 
 export const metadata = {
-  title: "Kontakt",
-  description: "Kontaktirajte nas za više informacija o betonskim ogradama.",
-  keywords: "kontakt, betonske ograde, Zrenjanin, Adexto",
+  title: "Kontakt - Betonske Ograde Zrenjanin",
+  description:
+    "Kontaktirajte nas za više informacija o našim betonskim ogradama u Zrenjaninu. Naša ekipa je tu da vam pomogne!",
+  keywords:
+    "kontakt betonske ograde, Zrenjanin kontakt, Adexto kontakt, betonske ograde Zrenjanin, info o betonskim ogradama",
   openGraph: {
-    title: "Kontakt - Adexto",
-    description: "Kontaktirajte nas za više informacija o betonskim ogradama.",
+    title: "Kontakt - Betonske Ograde Zrenjanin | Adexto",
+    description:
+      "Kontaktirajte nas za više informacija o našim betonskim ogradama u Zrenjaninu. Naša ekipa je tu da vam pomogne!",
     url: "https://adexto.com/kontakt",
     siteName: "Adexto",
     type: "website",
