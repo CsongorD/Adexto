4b9030ec1eb5ba7b47e0661f272309f31c42a755
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-04-01 21:21:56 +0200

    update cenovnik page metadata

--- src/app/cenovnik/page.js
+++ src/app/cenovnik/page.js
@@ -1,18 +1,20 @@
 import PriceTable from "../../components/PriceTable/PriceTable";
 
 export const metadata = {
-  title: "Cenovnik",
-  description: "Pogledajte naše cene za betonske ograde u Zrenjaninu.",
-  keywords: "cenovnik, betonske ograde, Zrenjanin, Adexto",
+  title: "Cenovnik - Betonske Ograde Zrenjanin",
+  description:
+    "Pogledajte ažurirane cene za betonske ograde u Zrenjaninu. Pristupačne cene i vrhunski kvalitet betonskih ograda za vaš dom ili poslovni prostor.",
+  keywords:
+    "cenovnik betonske ograde Zrenjanin, cene betonskih ograda, betonske ograde Adexto, betonske ograde Zrenjanin",
   openGraph: {
-    title: "Cenovnik - Adexto",
-    description: "Pogledajte naše cene za betonske ograde u Zrenjaninu.",
+    title: "Cenovnik - Betonske Ograde Zrenjanin | Adexto",
+    description:
+      "Pogledajte ažurirane cene za betonske ograde u Zrenjaninu. Pristupačne cene i vrhunski kvalitet betonskih ograda za vaš dom ili poslovni prostor.",
     url: "https://adexto.com/cenovnik",
     siteName: "Adexto",
     type: "website",
   },
 };
-
 
 export default function TypesPage() {
   return <PriceTable />;
