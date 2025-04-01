3f7d8d3f651600fcf94976b77fe631760c7f3968
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-04-01 21:22:24 +0200

    update modeli page metadata

--- src/app/modeli/page.js
+++ src/app/modeli/page.js
@@ -1,18 +1,20 @@
 import ModelTable from "../../components/ModelTable/ModelTable";
 
 export const metadata = {
-  title: "Modeli",
-  description: "Istražite različite modele betonskih ograda koje nudimo.",
-  keywords: "betonske ograde, modeli, dizajn, Zrenjanin, Adexto",
+  title: "Modeli - Betonske Ograde Zrenjanin",
+  description:
+    "Istražite različite modele betonskih ograda koje nudimo. Saznajte koji dizajn je najbolji za vaš prostor.",
+  keywords:
+    "modeli betonskih ograda, dizajn betonskih ograda, betonske ograde Zrenjanin, Adexto modeli ograda, vrste betonskih ograda",
   openGraph: {
-    title: "Modeli - Adexto",
-    description: "Istražite različite modele betonskih ograda koje nudimo.",
+    title: "Modeli - Betonske Ograde Zrenjanin | Adexto",
+    description:
+      "Istražite različite modele betonskih ograda koje nudimo. Saznajte koji dizajn je najbolji za vaš prostor.",
     url: "https://adexto.com/modeli",
     siteName: "Adexto",
     type: "website",
   },
 };
-
 
 export default function ModelsPage() {
   return <ModelTable />;
