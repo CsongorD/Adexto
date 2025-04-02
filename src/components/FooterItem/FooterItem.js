c00c5529b0dac88ae8e448679b6e23db4a8abb19
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-04-02 17:12:33 +0200

    remove footeritem aria label

--- src/components/FooterItem/FooterItem.js
+++ src/components/FooterItem/FooterItem.js
@@ -3,9 +3,7 @@ import styles from "./FooterItem.module.css";
 const FooterItem = ({ icon, title, info }) => {
   return (
     <div className={styles["info-container"]}>
-      <div className={styles["icon-container"]} aria-label={title}>
-        {icon}
-      </div>
+      <div className={styles["icon-container"]}>{icon}</div>
       <div className={styles["info-text"]}>
         <p className={styles["footer-info-title"]}>{title}</p>
         <p className={styles["footer-info"]}>{info}</p>
