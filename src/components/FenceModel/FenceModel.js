3ad02dbfa69f6ab8e027a58b006f965f20994978
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-04-05 18:50:03 +0200

    fix safari autoplay in fencemodel

--- src/components/FenceModel/FenceModel.js
+++ src/components/FenceModel/FenceModel.js
@@ -3,7 +3,7 @@ import styles from "./FenceModel.module.css";
 const FenceModel = () => {
   return (
     <div className={styles["fence-model-container"]}>
-      <video autoPlay muted>
+      <video autoPlay playsInline muted preload="auto">
         <source src="/adexto-3-d-model.mp4" type="video/mp4" />
       </video>
     </div>
