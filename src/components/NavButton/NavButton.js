dfb3fdec8617d107579cad4e4c2b0cdb62385d2c
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-04-10 21:06:29 +0200

    fix nav-button active status

--- src/components/NavButton/NavButton.js
+++ src/components/NavButton/NavButton.js
@@ -5,13 +5,8 @@ import styles from "../Navbar/Navbar.module.css";
 
 const NavButton = ({ to, text, onClick = () => {}, className = "" }) => {
   const pathname = usePathname(); // Replaces useRouter() for Next.js App Router
-  let isActive = false;
+  const isActive = pathname === to;
 
-  if (pathname === "/") {
-    isActive = pathname === to;
-  } else {
-    isActive = pathname.slice(0, -1) === to;
-  }
   const handleClick = (event) => {
     if (pathname === to) {
       event.preventDefault(); // Prevents reloading the same page
