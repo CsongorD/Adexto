2c19d6313a254c76e7f1c13bc59a3abd3325c084
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-08-30 18:07:23 +0200

    Make column cards bigger but keep them fully visible

--- src/components/ColumnList/ColumnList.js
+++ src/components/ColumnList/ColumnList.js
@@ -8,7 +8,7 @@ const ColumnList = () => {
   if (error) throw error;
 
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 max-w-5xl mx-auto">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-4xl mx-auto">
       {!columns ? (
         <div className="col-span-full flex justify-center py-12">
           <ImageLoading />
