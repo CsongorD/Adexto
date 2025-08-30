068c12f42c33eff44eb565081893ddb28004c222
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-08-30 20:05:20 +0200

    Remove selected effect from navigation buttons

--- src/components/NavButton/NavButton.js
+++ src/components/NavButton/NavButton.js
@@ -20,13 +20,7 @@ const NavButton = ({ to, text, onClick = () => {}, className = "" }) => {
   return (
     <Link
       href={to}
-      className={`${className} ${
-        isHeaderNav && isActive 
-          ? "text-primary-400 after:scale-x-100" 
-          : isHeaderNav 
-          ? "after:scale-x-0 hover:after:scale-x-100"
-          : ""
-      } ${isHeaderNav ? "relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-primary-400 after:transition-transform after:duration-300 after:origin-left" : ""}`}
+      className={className}
       onClick={handleClick}
     >
       {text}
