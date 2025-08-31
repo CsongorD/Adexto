7248a42e34fbde9a3a22fda83736900d93f8fa3c
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-08-31 18:24:11 +0200

    Fix duplicate export in FooterItem component

--- src/components/FooterItem/FooterItem.js
+++ src/components/FooterItem/FooterItem.js
@@ -19,24 +19,3 @@ const FooterItem = ({ icon, title, info }) => {
 };
 
 export default FooterItem;
-
-const FooterItem2 = ({ icon, title, info }) => {
-  return (
-    <div className="space-y-3 group">
-      <div className="flex items-center space-x-2 group-hover:text-primary-400 transition-colors duration-300">
-        <div className="flex-shrink-0">
-        {icon}
-      </div>
-      <div className="space-y-1">
-        <h4 className="text-white font-semibold">
-          {title}
-        </h4>
-        <p className="text-gray-400 text-sm leading-relaxed">
-          {info}
-        </p>
-      </div>
-    </div>
-  );
-};
-
-export default FooterItem;
